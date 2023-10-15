5d85e2b6ffafd8d4a31ab5e5feb6a1ff63aeb4b6
welnmoon <125731918+welnmoon@users.noreply.github.com>
2023-10-15 13:14:48 +0600

    Add files via upload

---
+++ Lab3/FixedCapacityStack/FixedCapacityStack.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "FixedCapacityStack.h"
+using namespace std;
+
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    FixedCapacityStack<int> mystack(5);
+
+    for (int i = 1; i <= 5; ++i) {
+        mystack.push(i);
+    }
+
+    cout << mystack;
+
+    if (!mystack.full()) {
+        mystack.push(6);
+    }
+    else {
+        cout << "Стэк полон!" << std::endl;
+    }
+    mystack.push(8);
+
+    return 0;
+}
+
+
---
+++ Lab3/FixedCapacityStack/FixedCapacityStack.h
@@ -0,0 +1,84 @@
+﻿#ifndef FIXEDCAPACITYSTACK_H
+#define FIXEDCAPACITYSTACK_H
+
+#include <iostream>
+template <typename T>
+class FixedCapacityStack {
+private:
+    T* a;
+    int n;
+    int capacity;
+
+public:
+    FixedCapacityStack(int capacity);
+    ~FixedCapacityStack();
+
+    bool empty() const;
+    bool full() const;
+    void push(T item);
+    T pop();
+    T top() const;
+
+    friend std::ostream& operator<<(std::ostream& os, const FixedCapacityStack<T>& stack) {
+        for (int i = stack.n - 1; i >= 0; --i) {
+            os << stack.a[i] << "\n";
+        }
+        return os;
+    }
+};
+
+template <typename T>
+FixedCapacityStack<T>::FixedCapacityStack(int capacity) : capacity(capacity), n(0) {
+    a = new T[capacity];
+}
+
+template <typename T>
+FixedCapacityStack<T>::~FixedCapacityStack() {
+    delete[] a;
+}
+
+template <typename T>
+bool FixedCapacityStack<T>::empty() const {
+    if (n == 0) {
+        return true;
+    }
+    else return false;
+}
+
+template <typename T>
+bool FixedCapacityStack<T>::full() const {
+    if (n == capacity) {
+        return true;
+    }
+    else return false;
+}
+
+template <typename T>
+void FixedCapacityStack<T>::push(T item) {
+    setlocale(LC_ALL, "ru");
+    if (!full()) {
+        a[n++] = item;
+    }
+    else {
+        cout<<"Стэк полный!";
+    }
+}
+
+template <typename T>
+T FixedCapacityStack<T>::pop() {
+    setlocale(LC_ALL, "ru");
+    if (!empty()) {
+        return a[--n];
+    }
+    cout<<"Стэк пустой!";
+}
+
+template <typename T>
+T FixedCapacityStack<T>::top() const {
+    setlocale(LC_ALL, "ru");
+    if (!empty()) {
+        return a[n - 1];
+    }
+   cout<<"Стэк пустой!";
+}
+#endif 
